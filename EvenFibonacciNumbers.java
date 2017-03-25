b952c6a0950b815b31642623b99dc5bcff3538e1
Akash Tomar <akash.tomar217@gmail.com>
2017-03-25 12:56:30 +0530

    dp with new fibonacci rule for even nos. only

--- EvenFibonacciNumbers.java
+++ EvenFibonacciNumbers.java
@@ -3,20 +3,21 @@ package projectEuler;
 import java.util.Scanner;
 
 public class EvenFibonacciNumbers {
-	static int[] fib;
+	static long[] fib;
 	
-	public static int fib(int index) {
+	public static long fib(int index) {
 		if(index==1) {
-			fib[index]=1;
-			return 1;
-		} else if(index==2) {
 			fib[index]=2;
 			return 2;
+		} else if(index==2) {
+			fib[index]=8;
+			return 8;
 		}
 		if(fib[index]!=0) {
 			return fib[index];
 		}
-		return fib(index-1)+fib(index-2);
+		fib[index]=4*fib(index-1)+fib(index-2);
+		return fib[index];
 	}
 	
 	public static void main(String[] args) {
@@ -24,17 +25,16 @@ public class EvenFibonacciNumbers {
 		Scanner s = new Scanner(System.in);
 		int t = s.nextInt();
 		for(int test=0;test<t;test++) {
-			int n = s.nextInt();
-			fib = new int[100000000];
+			long n = s.nextLong();
+			fib = new long[10000000];
 			int index=1;
 			long sum = 0;
 			while(true) {
-				int fibnum = fib(index);
+				long fibnum = fib(index);
 				if(fibnum>n) {
 					break;
-				} else if(fibnum%2==0){
-					sum+=fibnum;
-				}
+				} 
+				sum+=fibnum;	
 				index++;
 			}
 			System.out.println(sum);
