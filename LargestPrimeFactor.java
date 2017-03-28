606fbaca9721b09b33388d9bd622d6f52893b4b1
Akash Tomar <akash.tomar217@gmail.com>
2017-03-28 15:55:26 +0530

    check if prime optimised

---
+++ LargestPrimeFactor.java
@@ -0,0 +1,50 @@
+package projectEuler;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class LargestPrimeFactor {
+	public static boolean isPrime(int num){
+		if ( num > 2 && num%2 == 0 ) {
+	        return false;
+	    }
+	    int top = (int)Math.sqrt(num) + 1;
+	    for(int i = 3; i < top; i+=2){
+	        if(num % i == 0){
+	            return false;
+	        }
+	    }
+	    return true; 
+	}
+	
+	public static ArrayList<Integer> getFactors(int n,int i) {
+		if(n==1) {
+			ArrayList<Integer> temp = new ArrayList<>();
+			temp.add(1);
+			return temp;
+		}
+		ArrayList<Integer> list = new ArrayList<>(); 
+		if(n%i==0) {
+			 list.add(i);
+			 list.addAll(getFactors(n/i, i));
+		} else {
+			list.addAll(getFactors(n, ++i));
+		}
+		return list;
+	}
+	public static void main(String[] args) {
+		Scanner s  = new Scanner(System.in);
+		int t = s.nextInt();
+		for(int test=0;test<t;test++) {
+			int n = s.nextInt();
+			ArrayList<Integer> list = getFactors(n, 2);
+			for(int i=list.size()-2;i>=0;i--) {
+				if(isPrime(list.get(i))) {
+					System.out.println(list.get(i));
+					break;
+				}
+			}
+		}
+		s.close();
+	}
+}
